112677dc1b587d5b872d568c3129b24f92564041
Yi Wu <yiwu0b11@gmail.com>
2024-03-03 15:00:44 +0000

    update give() logic
    Before: opened player info menu does not close, causing an issue that in the next loop, it will close menu and fail to give like
    After: open and close player info menu in every loop

--- fallen_doll.py
+++ fallen_doll.py
@@ -102,7 +102,8 @@ def finish():
 def give():
     win = pyautogui.getWindowsWithTitle('FallenDoll')[0]
     x, y = win.left, win.top
-    pyautogui.moveTo(x + 339 * resRate, y + 280 * resRate + 35)
+    # open top player menu
+    pyautogui.moveTo(x + 339 * resRate, y + 280 * resRate + 35)  
     pyautogui.leftClick()
     wait(0.2)
     pyautogui.moveTo(x + 94 * resRate, y + 310 * resRate + 60)
@@ -111,6 +112,10 @@ def give():
     wait(0.2)
     pyautogui.leftClick()
     wait(0.2)
+    pyautogui.leftClick()
+    wait(0.2)
+    # close top player menu
+    pyautogui.moveTo(x + 339 * resRate, y + 280 * resRate + 35)
     pyautogui.leftClick()
     wait(0.2)
 
@@ -142,7 +147,8 @@ def loop():
         pyautogui.moveRel(50 * resRate, 50 * resRate)
         log('结束')
         wait(0.2)
-    give()
+    while give():
+        log('点赞')
 
 
 if __name__ == '__main__':
